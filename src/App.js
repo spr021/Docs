670cf82668a447a36ea79786fe4ffc5dfe1056b0
Saber Pourrahimi <saber@Sabers-MacBook-Pro.local>
2021-10-09 17:17:03 +0330

    add login with PacMan mod :D

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "./view/pages/Home"
 import Login from "./view/pages/Login"
 import NewDoc from "./view/pages/NewDoc"
